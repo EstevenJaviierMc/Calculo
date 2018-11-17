688c6baa553163cae3e8d1960c5c3412b74df1c7
EstevenJaviierMc <estevenjaviier@gmail.com>
2018-11-17 07:01:24 -0500

    ...

--- Calculo/Program.cs
+++ Calculo/Program.cs
@@ -8,39 +8,47 @@ namespace Calculo
 {
     class Program
     {
-
-        static double Fx1(double x)
+        static double trap(int n, double[] x, double[] fx, double t)
         {
-            return Math.Sin(x);
-        }
-
-        static double trapecio(double A, double B, int N)
-        {
-            double Integral = 0;
-            double h = (B - A) / N;
-            for (double x = A; x < B; x += h)
+            double suma = 0;
+            for (int i = 1; i < n; i++)
             {
-                double xdch = x + h;
-                double Fx = Fx1(x);
-                double Fxdch = Fx1(xdch);
-                Integral += (Fx + Fxdch) / 2 * h;
+                suma = suma + fx[i];
             }
-            return Integral;
+            double I = (x[n] - x[0]) * (fx[0] + 2 * suma + fx[n]) / (2 * t);
+            return I;
         }
+
         static void Main(string[] args)
         {
-            double Menor = 0.0, Mayor = Math.PI / 2.0;
-            int Intervalos = 1000;
-            double Ancho = (Mayor - Menor) / Intervalos;
-            double Integral = (Fx1(Menor) + Fx1(Mayor)) / 2.0;
-            for (int I = 1; I < Intervalos; I++)
-            {
-                Integral += Fx1(Menor + I * Ancho);
-            }
-            Integral *= Ancho;
+            Console.Write("ingrese numero de datos a analizar: ");
+            int num = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(Integral);
-            Console.ReadLine();
+            Console.Write("ingrese Ancho de los trapecios: ");
+            double h = double.Parse(Console.ReadLine());
+
+            int n = num;
+
+            var x = new double[n];
+            var fx = new double[n];
+
+            Console.WriteLine("Introduce los datos");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("x[" + i + "]= ");
+                x[i] = double.Parse(Console.ReadLine());
+
+                Console.Write("fx[" + i + "]= ");
+                fx[i] = double.Parse(Console.ReadLine());
+            }
+            //Ajueste respecto
+
+            n = num - 1;
+            double t = (x[n] - x[0]) / h;
+
+            Console.WriteLine("El valor de la integral es: " + trap(n, x, fx, t));
+
+            Console.ReadKey();
         }
     }
 }
