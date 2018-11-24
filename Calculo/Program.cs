75bd8a751d0103025a5e924f6df24f2c4fba4c59
EstevenJaviierMc <estevenjaviier@gmail.com>
2018-11-24 16:08:58 -0500

    Terminado

--- Calculo/Program.cs
+++ Calculo/Program.cs
@@ -4,52 +4,88 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Calculo
+namespace Calculo2
 {
     class Program
     {
+        //Se define metodo con el nombre TrapM y se le pasa los valores q tienen las variables "n", "nTrapecios" y los arreglos "x" y "f" 
         static double TrapM(int n, double[] x, double[] fx, double nTrapecios)
         {
+            //Se define la variable suma con un valor de 0
             double suma = 0;
 
+            //Se suman todos los valores q tiene el arreglo "fx" y se guarda en la variable "suma"
             for (int i = 1; i < n; i++)
             {
                 suma = suma + fx[i];
             }
 
             double I = (x[n] - x[0]) * (fx[0] + 2 * suma + fx[n]) / (2 * nTrapecios);
+
+            //el metodo retorna el valor de la variable "I"
             return I;
         }
 
         static void Main(string[] args)
         {
-            Console.Write("ingrese numero de datos a analizar: ");
-            int num = int.Parse(Console.ReadLine());
-
-            Console.Write("ingrese Ancho de los trapecios: ");
-            double h = double.Parse(Console.ReadLine());
-
-            int n = num;
-
-            double[] x = new double[n];
-            double[] fx = new double[n];
-
-            Console.WriteLine("Introduce los datos");
-            for (int i = 0; i < n; i++)
+            double b, a;
+            do
             {
-                Console.Write("x[" + i + "]= ");
-                x[i] = double.Parse(Console.ReadLine());
+                //Pide al usuario el limite superior y lo guarda en la variable b
+                Console.Write("ingrese limite superior: ");
+                b = double.Parse(Console.ReadLine());
 
-                Console.Write("fx[" + i + "]= ");
-                fx[i] = double.Parse(Console.ReadLine());
+
+                //Pide al usuario el limite superior y lo guarda en la variable a
+                Console.Write("ingrese limite inferior: ");
+                a = double.Parse(Console.ReadLine());
+
+                if (a > b)
+                {
+                    Console.Clear();
+                    Console.WriteLine("El limite superior debe se mayor al limite inferior!");
+                }
+
+            } while (a > b);
+
+
+            //Pide al usuario el limite el numero de datos a analizar y se guarda en la variable n
+            Console.Write("ingrese numero de datos a analizar: ");
+            int n = int.Parse(Console.ReadLine());
+
+            //Se define el arreglo x 
+            double[] x = new double[n + 1];
+
+            //Se le da valor a H, restando el limite superior con el limite inferior y dividiendo el resultado entre n
+            double h = (b - a) / n;
+
+            //Se le asigna el valor del limite inferior en la posicion 0 del arreglo x
+            //Y el valor del limite superior en la posicion n del arreglo
+            x[0] = a;
+            x[n] = b;
+
+            //Asignandole valores a x2, x3 .... fn-1
+            for (int i = 1; i < n; i++)
+            {
+                x[i] = x[i - 1] + h;
             }
-            //Ajueste respecto a la posicion que ocupa en el arreglo los valores
 
-            n = num - 1;
+            //Se define el arreglo fx
+            double[] fx = new double[n + 1];
+
+            //Asignandole valores a f1,f2, .... fn
+            for (int i = 0; i < fx.Length; i++)
+            {
+                //Funcion que se nos asigno
+                //fx[i]= Math.Pow( 8.1 + 12.5 * Math.Pow(x[i], 6) + 0.6 * x[i] + 10 * Math.Pow(x[i], 4) ,2);
+
+                //Funcion que se da de ejemplo en el pdf dado por el profesor
+                fx[i] = 0.2 + 25 * x[i] - 200 * Math.Pow(x[i], 2) + 675 * Math.Pow(x[i], 3) - 900 * Math.Pow(x[i], 4) + 400 * Math.Pow(x[i], 5);
+            }
+
             double nTrapecios = (x[n] - x[0]) / h;
 
-
-            Console.WriteLine("El valor de la integral es: " + TrapM(n, x, fx, nTrapecios));
+            Console.WriteLine("El valor de la integral es: " + TrapM(n, x, fx, nTrapecios));//Se ejecuta el metodo TrapM
 
             Console.ReadKey();
         }
