03c885cf2c269a6ca094ad47650f3334d16d4875
EstevenJaviierMc <estevenjaviier@gmail.com>
2018-11-25 08:12:45 -0500

    detalles

--- Calculo/Program.cs
+++ Calculo/Program.cs
@@ -8,20 +8,20 @@ namespace Calculo2
 {
     class Program
     {
-        //Se define metodo con el nombre TrapM y se le pasa el valor q tienen la variable "n" y los arreglos "x" y "fx", para calcular la integral
-        static double TrapM(int n, double[] x, double[] fx)
+        //Se define metodo con el nombre TrapM y se le pasa el valor q tienen la variable "n" y los arreglos "x" y "f", para calcular la integral
+        static double TrapM(int n, double[] x, double[] f)
         {
             //Se define la variable suma con un valor de 0
             double suma = 0;
 
-            //Se suman todos los valores q tiene el arreglo "fx" y se guarda en la variable "suma"
+            //Se suman todos los valores q tiene el arreglo "f" y se guarda en la variable "suma"
             for (int i = 1; i < n; i++)
             {
-                suma = suma + fx[i];
+                suma = suma + f[i];
             }
 
             //Se calcula la integral mediante la formula y se guarda en la variable I
-            double I = (x[n] - x[0]) * (fx[0] + 2 * suma + fx[n]) / (2 * n);
+            double I = (x[n] - x[0]) * (f[0] + 2 * suma + f[n]) / (2 * n);
 
             //el metodo retorna el valor de la variable "I"
             return I;
@@ -68,23 +68,26 @@ namespace Calculo2
             //Asignandole valores a x2, x3 .... fn-1
             for (int i = 1; i < n; i++)
             {
+                //se suma el valor q esta en la posicion cero mas el valor de altura "h", y se guarda en la posicion 1 del arreglo
                 x[i] = x[i - 1] + h;
             }
 
-            //Se define el arreglo fx
-            double[] fx = new double[n + 1];
+            //Se define el arreglo f
+            double[] f = new double[n + 1];
 
             //Asignandole valores a f1,f2, .... fn
-            for (int i = 0; i < fx.Length; i++)
+            for (int i = 0; i < f.Length; i++)
             {
                 //Funcion que se nos asigno
-                //fx[i]= Math.Pow( 8.1 + 12.5 * Math.Pow(x[i], 6) + 0.6 * x[i] + 10 * Math.Pow(x[i], 4) ,2);
+                //f[i]= Math.Pow( 8.1 + 12.5 * Math.Pow(x[i], 6) + 0.6 * x[i] + 10 * Math.Pow(x[i], 4) ,2);
 
                 //Funcion que se da de ejemplo en el pdf dado por el profesor
-                fx[i] = 0.2 + 25 * x[i] - 200 * Math.Pow(x[i], 2) + 675 * Math.Pow(x[i], 3) - 900 * Math.Pow(x[i], 4) + 400 * Math.Pow(x[i], 5);
+                f[i] = 0.2 + 25 * x[i] - 200 * Math.Pow(x[i], 2) + 675 * Math.Pow(x[i], 3) - 900 * Math.Pow(x[i], 4) + 400 * Math.Pow(x[i], 5);
             }
 
-            Console.WriteLine("El valor de la integral es: " + TrapM(n, x, fx));//Se ejecuta el metodo TrapM
+            Console.WriteLine();
+            Console.WriteLine("El valor de la integral es: " + TrapM(n, x, f));//Se ejecuta el metodo TrapM
+
 
             Console.ReadKey();
         }
